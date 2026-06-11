ea8d1d16e78389e94cc20c043d182e7fac3ecf0a
Muhammad Kashif <154686104+Muhammadkashif20@users.noreply.github.com>
2026-06-11 10:28:41 +0500

    resolving dns issue in mongodb and removing extra code

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import dns from "dns";
-dns.setServers(["1.1.1.1","8.8.8.8"])
+dns.setServers(["1.1.1.1", "8.8.8.8"])
 import authRouter from "./routers/auth.js";
 dotenv.config();
+
 const app = express();
 const PORT = 4000;
 app.use(cors());
 app.use(express.json());
+
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
@@ -18,7 +20,7 @@ mongoose
   .catch((err) => {
     console.log("Error connecting to MongoDB:", err);
   });
-app.use("/api/auth",authRouter)
+app.use("/api/auth", authRouter)
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
