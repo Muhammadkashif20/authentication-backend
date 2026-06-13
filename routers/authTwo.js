d4cf8c4955f160ccd0780848cd85ee2322940e19
Muhammad Kashif <154686104+Muhammadkashif20@users.noreply.github.com>
2026-06-13 13:35:39 +0500

    api hit issue

--- routers/authTwo.js
+++ routers/authTwo.js
@@ -2,22 +2,22 @@ import express from "express";
 const router = express.Router();
 import Joi from "joi"
 // Validation Register Schema
-const signUp=Joi.object({
-    "fullname":Joi.string().min(6).required(),
-    "email":Joi.string().email().required(),
-    "password":Joi.string().min(6).required(),
+const signUp = Joi.object({
+    "fullname": Joi.string().min(6).required(),
+    "email": Joi.string().email().required(),
+    "password": Joi.string().min(6).required(),
 })
-const login=Joi.object({
-    "email":Joi.string().email().required(),
-    "password":Joi.string().min(6).required(),
+const login = Joi.object({
+    "email": Joi.string().email().required(),
+    "password": Joi.string().min(6).required(),
 })
-router.post("/signup",(req,res)=>{
-  try {
-    const {error,value}=signUp.validate(req.body)
-    console.log("req.body",req.body)
-    res.status().json()
-  } catch (error) {
-    console.log("error",error)
-  } 
+router.post("/signup",(req, res) => {
+    try {
+        const { error, value } = signUp.validate(req.body)
+        console.log("req.body", req.body)
+        res.status(201).json({ message: "User created successfully" });
+    } catch (error) {
+        console.log("error", error)
+    }
 })
 export default router;
